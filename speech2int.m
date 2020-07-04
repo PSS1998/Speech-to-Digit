ea4bb5e56b1d11c2fbe53b3438634a51679c51fa
PSS1998 <p_sadri_s@yahoo.com>
2020-07-04 20:00:21 +0430

    Update speech2int.m

--- speech2int.m
+++ speech2int.m
@@ -5,17 +5,15 @@ function y = speech2int(digits, speech)
         temp = x(:,i);
         diff = [];
         for j = 1:10
-            R = corr2(temp,digits(:,j));
+            [r,lags] = xcorr(temp,digits(:,j));
+            idx = find(r==max(r));
+            temp_digit = circshift(digits(:,j),lags(:,idx));
+            R = corr2(temp_digit,temp);
             diff = [diff; R];
         end
         diff;
-        max(diff)
+        max(diff);
         idx = find(diff==max(diff))-1;
         y = y + 10^(size(x,2)-i)*idx;
     end
-    %sound(x(:,3),8000);
-    %figure(1)
-    %plot(x(:,3))
-    %figure(2)
-    %plot(digits(:,2))
 end
